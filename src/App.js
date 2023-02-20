175e8c289b62e6b7374093120a3edeb08152343b
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2023-02-20 20:14:10 +0900

    organisms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atom/button/PrimaryButton";
 import { SecondaryButton } from "./components/atom/button/SecondaryButton";
 import { SerchInput } from "./components/molecules/SerchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "じゃけぇ",
+  image: "https://source.unsplash.com/BJaqPaH6AGQ",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SerchInput />
+      <UserCard user={user} />
     </div>
   );
 }
